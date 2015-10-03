6dbcae558b709b49afbe44e255726992bc92e446
Alex Eyler <alex.eyler@outlook.com>
2015-10-03 13:03:41 -0700

    Initial commit

---
+++ CopyAsPathCommand/CopyAsPathCommand.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel.Design;
+using Microsoft.VisualStudio.Shell;
+using Microsoft;
+using EnvDTE;
+using System.Windows;
+
+namespace CopyAsPathCommand
+{
+    internal sealed class CopyAsPathCommand
+    {
+        public const int CommandId = 0x0100;
+        public static readonly Guid CommandSet = new Guid("cceebeea-e540-447e-8b6d-ffec0ffd311d");
+
+        private UIHierarchy solutionExplorer;
+
+        private CopyAsPathCommand(IServiceProvider serviceProvider, UIHierarchy solutionExplorer)
+        {
+            Requires.NotNull(serviceProvider, nameof(serviceProvider));
+            Requires.NotNull(solutionExplorer, nameof(solutionExplorer));
+
+            OleMenuCommandService commandService = serviceProvider.GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            if (commandService != null)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+
+            this.solutionExplorer = solutionExplorer;
+        }
+
+        public static CopyAsPathCommand Instance { get; private set; }
+
+        public static void Initialize(IServiceProvider serviceProvider, UIHierarchy solutionExplorer)
+        {
+            Requires.NotNull(serviceProvider, nameof(serviceProvider));
+            Requires.NotNull(solutionExplorer, nameof(solutionExplorer));
+            Instance = new CopyAsPathCommand(serviceProvider, solutionExplorer);
+        }
+
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            Array selectedItems = this.solutionExplorer.SelectedItems as Array;
+            if (selectedItems != null)
+            {
+                foreach (UIHierarchyItem item in selectedItems)
+                {
+                    ProjectItem prjItem = item.Object as ProjectItem;
+                    string path = "\"" + prjItem.Properties.Item("FullPath").Value.ToString() + "\"";
+                    Clipboard.SetText(path);
+                }
+            }
+        }
+    }
+}
---
+++ CopyAsPathCommand/CopyAsPathCommandPackage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+
+namespace CopyAsPathCommand
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [InstalledProductRegistration(CopyAsPathCommandPackage.ProductName,
+        CopyAsPathCommandPackage.ProductDescription, "1.0")]
+    [Guid(CopyAsPathCommandPackage.PackageGuidString)]
+    public sealed class CopyAsPathCommandPackage : Package
+    {
+        public const string ProductName = "Copy as path command";
+        public const string ProductDescription = "Right click on a solution explorer item and get its absolute path.";
+        public const string PackageGuidString = "d40a488d-23d0-44cc-99fb-f6dd0717ab7d";
+
+        #region Package Members
+        protected override void Initialize()
+        {
+            DTE2 envDte = this.GetService(typeof(DTE)) as DTE2;
+            UIHierarchy solutionWindow = envDte.ToolWindows.SolutionExplorer;
+            CopyAsPathCommand.Initialize(this, solutionWindow);
+            base.Initialize();
+        }
+        #endregion
+    }
+}
