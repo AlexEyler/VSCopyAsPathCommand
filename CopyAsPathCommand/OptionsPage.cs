d782cf5c4dceef4d991843a920bb64f9e172726a
Alex Eyler <alex.eyler@outlook.com>
2015-10-03 18:30:30 -0700

    Add option for including quotes

---
+++ CopyAsPathCommand/OptionsPage.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyAsPathCommand
+{
+    internal class OptionsPage : DialogPage
+    {
+        internal const bool DefaultQuotesOption = true;
+        private bool? quotesOption;
+
+        [Category("Command Options")]
+        [DisplayName("Include quotes in path")]
+        [Description("If true, quotes will surround the copied path.")]
+        public bool QuotesOption
+        {
+            get { return this.quotesOption ?? OptionsPage.DefaultQuotesOption; }
+            set { this.quotesOption = value; }
+        }
+    }
+}
