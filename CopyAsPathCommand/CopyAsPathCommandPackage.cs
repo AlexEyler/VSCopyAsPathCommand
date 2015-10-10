c7407e2b358551ce3448fd6af2c420a81cd8f123
AlexEyler <alex.eyler@outlook.com>
2015-10-10 14:35:09 -0700

    Add Version string to package for support for yaml script

--- CopyAsPathCommand/CopyAsPathCommandPackage.cs
+++ CopyAsPathCommand/CopyAsPathCommandPackage.cs
@@ -10,12 +10,13 @@ namespace CopyAsPathCommand
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [InstalledProductRegistration(CopyAsPathCommandPackage.ProductName,
-        CopyAsPathCommandPackage.ProductDescription, "1.0")]
+        CopyAsPathCommandPackage.ProductDescription, CopyAsPathCommandPackage.Version)]
     [Guid(CopyAsPathCommandPackage.PackageGuidString)]
     [ProvideAutoLoad(UIContextGuids.SolutionExists)]
     [ProvideOptionPage(typeof(OptionsPage), "Environment", "Copy as Path Command", 0, 0, true)]
     public sealed class CopyAsPathCommandPackage : Package
     {
+        public const string Version = "1.0.6";
         public const string ProductName = "Copy as path command";
         public const string ProductDescription = "Right click on a solution explorer item and get its absolute path.";
         public const string PackageGuidString = "d40a488d-23d0-44cc-99fb-f6dd0717ab7d";
